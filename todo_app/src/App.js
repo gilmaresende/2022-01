512a028ba28f27daa909b96f3245587f6979aa4c
Gilmar <gilmar74796@gmail.com>
2022-02-10 23:54:04 -0300

    Start AppTodo

---
+++ todo_app/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+class TodoApp extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Nosso TodoApp</h1>
+        <input />
+        <button>Salvar</button>
+      </div>
+    );
+  }
+}
+export default TodoApp;
