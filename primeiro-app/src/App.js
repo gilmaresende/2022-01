0163e2f9ef9cb24f97d92790839d0cc70ae2b998
Gilmar <gilmaresende@gmail.com>
2023-05-22 16:50:24 -0300

    Manipulando Formularios

--- primeiro-app/src/App.js
+++ primeiro-app/src/App.js
@@ -2,17 +2,51 @@ import { useState } from 'react'
 
 function App() {
 
-  const [aluno, setAluno] = useState('Sujeito Dev')
+  const [nome, setNome] = useState('')
+  const [email, setEmail] = useState('')
+  const [idade, setIdade] = useState(0)
+
+  const [user, setUser] = useState({});
+
+
   return (
     <div >
-      <h1>Component App</h1>
-      <h2>Olá: {aluno}</h2>
-      <button onClick={() => handleChangerName()}>Mudar Nome</button>
+      <form onSubmit={handleRegister}>
+        <label>Nome:</label><br />
+        <input
+          placeholder='Digite seu nome:'
+          value={nome}
+          onChange={(e) => setNome(e.target.value)} /><br />
+        <label>e-mail:</label><br />
+        <input
+          placeholder='Digite seu e-mail:'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)} /><br />
+        <label>Idade:</label><br />
+        <input
+          placeholder='Digite seu idade:'
+          value={idade}
+          onChange={(e) => setIdade(e.target.value)}
+        /><br />
+        <button type='submit'> Registrar</button>
+      </form>
+      <br />
+      <br />
+      <div>
+        <span>Bem Vindo: {user.nome}</span><br />
+        <span>Idade: {user.email}</span><br />
+        <span>e-mail: {user.idade}</span><br />
+      </div>
     </div>
   );
 
-  function handleChangerName() {
-    setAluno("Dev Super")
+  function handleRegister(e) {
+    e.preventDefault();
+    setUser({
+      nome,
+      idade,
+      email
+    })
   }
 }
 
