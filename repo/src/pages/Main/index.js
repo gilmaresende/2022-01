4f86819d4869b69ded32f8007abf663e1d4dc7a0
Gilmar <gilmaresende@gmail.com>
2023-05-25 00:25:57 -0300

    Estilizando pagina main

--- repo/src/pages/Main/index.js
+++ repo/src/pages/Main/index.js
@@ -1,5 +1,19 @@
-import { Title } from "./styles";
+import { Container, Form, SubmitButton } from "./styles";
+import { FaGithub, FaPlus } from 'react-icons/fa'
 
 export default function Main() {
-   return <><Title>Hello World</Title></>
+   return <Container>
+      <h1>
+         <FaGithub size={25} />
+         Meus Repositorios
+      </h1>
+
+      <Form onSubmit={() => { }}>
+         <input type="text" placeholder="Adicionar Repositorio" />
+         <SubmitButton>
+            <FaPlus color="#fff" size={14} ></FaPlus>
+
+         </SubmitButton>
+      </Form>
+   </Container>
 }
