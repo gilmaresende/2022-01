aedba9b763b9f045c941944a9d7490934e8c99f4
Gilmar <gilmaresende@gmail.com>
2023-05-27 20:39:53 -0300

    Carregando Detalhes

--- repo/src/pages/Repositorio/index.js
+++ repo/src/pages/Repositorio/index.js
@@ -1,5 +1,30 @@
+import api from "../../services/api";
+import { Cantainer } from "./style";
+import { useEffect, useState } from "react";
+
 export default function Repositorio({ match }) {
-   return <>
-      <h1 style={{ color: "#fff" }}>Repositório {decodeURIComponent(match.params.repositorio)}</h1>
-   </>
+
+   const [repositorio, setRepositorio] = useState({})
+   const [issues, setUssues] = useState([])
+   const [loading, setLoading] = useState(false)
+
+   useEffect(() => {
+      async function load() {
+         setLoading(true)
+         const nomeRepo = decodeURIComponent(match.params.repositorio)
+
+         const [repositorioData, issuesData] = await Promise.all([
+            api.get(`repos/${nomeRepo}`),
+            api.get(`repos/${nomeRepo}/issues`)
+         ])
+
+         setRepositorio(repositorioData.data)
+         setUssues(issuesData.data)
+         setLoading(false)
+      }
+
+      load()
+   }, [])
+
+   return <Cantainer>dev</Cantainer>
 }
---
+++ repo/src/pages/Repositorio/style.js
@@ -0,0 +1,3 @@
+import styled, { keyframes, css } from 'styled-components'
+
+export const Cantainer = styled.div``
