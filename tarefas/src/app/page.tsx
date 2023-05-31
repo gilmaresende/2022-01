d78dbc02e9a317190827dc0d31db68e6883f985e
Gilmar <gilmaresende@gmail.com>
2023-05-31 00:47:40 -0300

    Criando projeto

--- tarefas/src/app/page.tsx
+++ tarefas/src/app/page.tsx
@@ -1,7 +1,24 @@
-
+import Head from "next/head";
+import styles from '../styles/home.module.css'
+import Image from "next/image";
+import heroImg from '../../public/assets/hero.png'
 
 export default function Home() {
   return (
-    <>dev</>
+    <div className={styles.container}>
+      <Head>
+        <title>Tarefas+ | Organize suas tarefas de forma fácil</title>
+      </Head>
+      <main className={styles.main}>
+        <div className={styles.logoContent}>
+          <Image
+            className={styles.hero}
+            src={heroImg} alt={"Logo Tarefa"}
+            priority
+          />
+        </div>
+        <h1 className={styles.title}>Sistema feito para você organizar<br /> seus estudos e tarefas</h1>
+      </main>
+    </div>
   )
 }
