877de3ff024ce68be28f3ddb90948c32955a89eb
Gilmar <gilmaresende@gmail.com>
2023-06-05 23:16:00 -0300

    criando login

--- tarefa/src/pages/_app.tsx
+++ tarefa/src/pages/_app.tsx
@@ -1,16 +1,17 @@
 import { Header } from '@/components/header'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { SessionProvider } from "next-auth/react"
 
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (<>
+  return (<SessionProvider session={pageProps.session}>
     <div className={inter.className}>
-      <Header></Header>
+      <Header/>
       <Component {...pageProps} />
     </div>
-  </>)
+  </SessionProvider>)
 }
