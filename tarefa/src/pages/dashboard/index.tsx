8015482e60975af67bb31dc876556a687f822c77
Gilmar <gilmaresende@gmail.com>
2023-06-08 17:07:14 -0300

    criando rota admin

--- tarefa/src/pages/dashboard/index.tsx
+++ tarefa/src/pages/dashboard/index.tsx
@@ -1,5 +1,7 @@
+import { GetServerSideProps } from 'next';
 import styles from './styles.module.css';
 import Head from 'next/head';
+import { getSession } from 'next-auth/react';
 export default function DashBoard() {
    return (
       <div className={styles.container}>
@@ -9,4 +11,21 @@ export default function DashBoard() {
          <h1>Pagina Painel</h1>
       </div>
    )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+
+   const session = await getSession({ req })
+
+   if (!session?.user) {
+      return {
+         redirect: {
+            destination: "/",
+            permanent: false
+         }
+      }
+   }
+   return {
+      props: {}
+   }
 }
